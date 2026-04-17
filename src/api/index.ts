ae0a33a8e2b8ffc7dcfaad5fba2bfe07c177562a
artgas1 <45360663+artgas1@users.noreply.github.com>
2026-04-17 19:08:48 +0300

    feat: add container-registry + dedicated-servers tools
    Container Registry (7 tools):
    - list_container_registries
    - get_container_registry
    - create_container_registry (preset_id XOR configurator_id+disk)
    - update_container_registry
    - delete_container_registry
    - list_container_registry_presets
    - list_container_registry_repositories
    
    Dedicated Servers (7 tools):
    - list_dedicated_servers
    - get_dedicated_server
    - create_dedicated_server
    - update_dedicated_server
    - delete_dedicated_server
    - list_dedicated_server_presets (optional location filter)
    - list_dedicated_server_additional_services

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,5 @@ export { appsApiClient } from "./apps";
 export { dbaasApiClient } from "./dbaas";
 export { serversApiClient } from "./servers";
 export { accountApiClient } from "./account";
+export { containerRegistryApiClient } from "./container-registry";
+export { dedicatedServersApiClient } from "./dedicated-servers";
